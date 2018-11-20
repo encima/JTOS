d9ca9bbf1eb34aef90e96f26961ca6fa70be3860
encima <encima@gmail.com>
2018-11-20 02:48:15 +0100

    support for limit and offset in query so it does not need to be specified in routes

--- jtos.py
+++ jtos.py
@@ -1,16 +1,16 @@
 class JTOS:
 
     mappings = {
-        'gt': '>',
-        'lt': '<',
-        'gte': '>=',
-        'lte': '<=',
-        'e': '=',
-        'ne': '!=',
-        'l': 'LIKE',
-        'nl': 'NOT LIKE',
-        'a': 'AND',
-        'o': 'OR'
+        "gt": ">",
+        "lt": "<",
+        "gte": ">=",
+        "lte": "<=",
+        "e": "=",
+        "ne": "!=",
+        "l": "LIKE",
+        "nl": "NOT LIKE",
+        "a": "AND",
+        "o": "OR",
     }
 
     def __init__(self):
@@ -21,13 +21,16 @@ class JTOS:
 
     def parseObject(self, obj):
         query_string = ""
-        if 'select' in obj:
-            query_string += self.buildSelect(obj['select'])
-        if 'where' in obj:
-            query_string += self.buildWhere(obj['where'])
-        if 'orderBy' in obj['select']:
-            query_string += self.buildOrder(obj['select']['orderBy'])
-
+        if "select" in obj:
+            query_string += self.buildSelect(obj["select"])
+        if "where" in obj:
+            query_string += self.buildWhere(obj["where"])
+        if "orderBy" in obj["select"]:
+            query_string += self.buildOrder(obj["select"]["orderBy"])
+        if 'limit' in obj:
+            query_string += ' LIMIT {}'.format(obj['limit'])
+        if 'offset' in obj:
+            query_string += ' OFFSET {}'.format(obj['offset'])
         return query_string.replace("  ", " ") + ";"
 
     def buildWhere(self, where_object):
@@ -35,21 +38,28 @@ class JTOS:
         for w, v in where_object.items():
             join = None
             if len(clauses) > 0:
-                join = 'AND' if not 'join' in v else v['join']
-            val = "'{}'".format(v['val']) if not isinstance(v['val'], int) else v['val'] 
-            clauses.append(" {0} {1} {2} {3}".format(JTOS.mappings[join] if join in JTOS.mappings else "", w, JTOS.mappings[v['op']], val))
+                join = "AND" if not "join" in v else v["join"]
+            val = "'{}'".format(v["val"]) if not isinstance(v["val"], int) else v["val"]
+            clauses.append(
+                " {0} {1} {2} {3}".format(
+                    JTOS.mappings[join] if join in JTOS.mappings else "",
+                    w,
+                    JTOS.mappings[v["op"]],
+                    val,
+                )
+            )
         return " WHERE" + "".join(clauses)
 
     def buildSelect(self, select_object):
-        f = ",".join(select_object['fields'])
-        t = ",".join(select_object['tables'])
-        select = "SELECT {0} FROM {1}".format(f,t)
+        f = ",".join(select_object["fields"])
+        t = ",".join(select_object["tables"])
+        select = "SELECT {0} FROM {1}".format(f, t)
         # TODO handle Joins
         return select
 
     def buildOrder(self, order_object):
         fields = []
-        for k,v in order_object.items():
+        for k, v in order_object.items():
             fields.append("{0} {1}".format(k, v.upper()))
         return " ORDER BY " + ", ".join(fields)
 
@@ -65,6 +75,7 @@ class JTOS:
     def buildCreate(self, create_object):
         pass
 
+
 # TODO auth against existing models to prevent malicious intent
 
 
@@ -79,7 +90,7 @@ class JTOS:
 #             ""
 #         ],
 #         orderBy: {
-#            "field": "ASC" 
+#            "field": "ASC"
 #         }
 #     }
 #     where: {
--- test_sql.py
+++ test_sql.py
@@ -1,68 +1,27 @@
 import jtos
 import pytest
 
+
 class TestJTOS:
 
     sw_obj = {
-        'select': {
-            'tables': [
-                "users"
-            ],
-            'fields': [
-                "email",
-                "id",
-                "password"
-            ]
+        "select": {"tables": ["users"], "fields": ["email", "id", "password"]},
+        "where": {
+            "email": {"op": "l", "val": "t@test.com"},
+            "name": {"op": "e", "val": "test", "join": "a"},
+            "id": {"op": "e", "val": 3, "join": "o"},
         },
-        'where': {
-            'email': {
-                'op': 'l',
-                'val': 't@test.com'
-            },
-            'name': {
-                'op': 'e',
-                'val': 'test',
-                'join': 'a'
-            },
-            'id': {
-                'op': 'e',
-                'val': 3,
-                'join': 'o'
-            }
-        }
     }
 
-    s_obj = {
-        'select': {
-            'tables': [
-                "users"
-            ],
-            'fields': [
-                "email",
-                "id",
-                "password"
-            ]
-        }
-    }
+    s_obj = {"select": {"tables": ["users"], "fields": ["email", "id", "password"]}}
 
     so_obj = {
-        'select': {
-            'tables': [
-                "users"
-            ],
-            'fields': [
-                "email",
-                "id",
-                "password"
-            ],
-            'orderBy': {
-                'email': 'ASC',
-                'id': 'desc'
-            }
+        "select": {
+            "tables": ["users"],
+            "fields": ["email", "id", "password"],
+            "orderBy": {"email": "ASC", "id": "desc"},
         }
     }
-
-
 
     def test_creation(self):
         j = jtos.JTOS()
@@ -72,20 +31,21 @@ class TestJTOS:
         j = jtos.JTOS()
         stmt = j.parseObject(TestJTOS.sw_obj)
         print(stmt)
-        assert stmt == "SELECT email,id,password FROM users WHERE email LIKE 't@test.com' AND name = 'test' OR id = 3;"
+        assert (
+            stmt
+            == "SELECT email,id,password FROM users WHERE email LIKE 't@test.com' AND name = 'test' OR id = 3;"
+        )
 
     def test_select_order(self):
         j = jtos.JTOS()
         stmt = j.parseObject(TestJTOS.so_obj)
         print(stmt)
-        assert stmt == "SELECT email,id,password FROM users ORDER BY email ASC, id DESC;"
+        assert (
+            stmt == "SELECT email,id,password FROM users ORDER BY email ASC, id DESC;"
+        )
 
     def test_select(self):
         j = jtos.JTOS()
         stmt = j.parseObject(TestJTOS.s_obj)
         print(stmt)
         assert stmt == "SELECT email,id,password FROM users;"
-
-    
-
-
